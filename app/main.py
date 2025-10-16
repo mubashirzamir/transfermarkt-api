909f8a5fec9178f8c643a10793b8aba91390759e
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-10-16 17:38:03 +0100

    chore: Allowed OPTIONS method.

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 app.include_router(api_router)
